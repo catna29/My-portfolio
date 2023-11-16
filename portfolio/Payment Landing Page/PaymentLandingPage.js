584da484d8d6149f56b755e4496b873d4b5fc0b6
cattareeya chernyshkin <cattareeya chernyshkin>
2023-11-15 16:17:22 -0800

    flie

---
+++ portfolio/Payment Landing Page/PaymentLandingPage.js
@@ -0,0 +1,15 @@
+
+/*Js top bar*/
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+
+    if (x.className === "topnav"){
+        x.className += " responsive";
+        y
+
+    } else {
+      x.className = "topnav";
+      
+    }
+  }
+
